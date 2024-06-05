998f447c3042db5b0f18fa8c0002744e3cb0d71f
aish <toshiparihar121212@gmail.com>
2024-06-05 13:38:20 +0530

    add download button

--- client/src/components/content/Pdfdownload.jsx
+++ client/src/components/content/Pdfdownload.jsx
@@ -1,26 +1,39 @@
 import React, { useState } from 'react';
 import "../../contact.css";
-
+import form from "../assets/form-pdf.pdf";
 
 const Pdfdownload = () => {
   const [message, setMessage] = useState('');
 
-  const handleDownload = () => {
-    let link = document.createElement('a');
-    link.href = "../asssets/form-pdf.pdf";
-    link.download = "form-pdf.pdf";
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const onButtonClick = () => {
+    // Show the message when download starts
     setMessage('Your download has started');
-  }
+
+    // Using JavaScript method to get PDF file
+    fetch(form).then((response) => {
+      response.blob().then((blob) => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        
+        // Setting various property values
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "form-pdf.pdf"; // Corrected download attribute
+        alink.click();
+      });
+    });
+  };
 
   return (
-    <div className='container-fluid text-center'>
-        
-      <button className='downladbtn' onClick={handleDownload}>Download PDF</button>
-      {message && <p className='alrtmsg'>{message}</p>}
-    </div>
+    <>
+      <center>
+        <h3>Download your form from here</h3>
+        <button className='downladbtn' onClick={onButtonClick}>
+          Download PDF
+        </button>
+        {message && <p className='alrtmsg'>{message}</p>}
+      </center>
+    </>
   );
 };
 
